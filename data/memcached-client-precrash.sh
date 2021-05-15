4ceffe7809e6d05cca0712335aeb9fa01615fc05
Hamed Gorjiara <hgorjiar@uci.edu>
2021-05-15 12:58:42 -0700

    Edit

--- data/memcached-client-precrash.sh
+++ data/memcached-client-precrash.sh
@@ -5,7 +5,5 @@ set -e
 BENCHMARKDIR=/scratch/nvm/redis-nvml
 ######################################### Running Jaaru to find bugs
 cd $BENCHMARKDIR
-cp run.sh run2.sh
 # Run client
-sed -i '6s/export PMCheck.*/export PMCheck="-d/ramfs/redis.pm -x1 -p1 -y -e -r2000"/' run2.sh
-./testcase/redistestcase.sh 1 | ./run2.sh ./src/redis-cli
+./testcase/memcachedtestcase.sh 1 | telnet localhost 11212
